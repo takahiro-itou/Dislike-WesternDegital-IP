f8e943f328ca201e2d6feb4395fd2c7dd9effad7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-10-20 20:32:35 +0900

    不具合修正

--- Rebuild.bat
+++ Rebuild.bat
@@ -3,4 +3,4 @@ msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x64  ^
     SampleView.sln
 
 msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x86  ^
-    SampleView.NetOld.sln
+    SampleView.sln
